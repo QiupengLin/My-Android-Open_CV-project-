7220c2aadddba744f493774f60e80d33915d5124
Qiupeng Lin <qiupeng.lin.87@gmail.com>
2018-02-15 05:12:42 -0600

    Feb.15th

--- app/src/main/java/com/example/qiupenglin/independent_study/HomeActivity.java
+++ app/src/main/java/com/example/qiupenglin/independent_study/HomeActivity.java
@@ -9,18 +9,31 @@ import android.widget.Button;
 public class HomeActivity extends AppCompatActivity {
 
     public static final int MEAN_BLUR = 1;
+    public static final int ERODE = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Button bMean;
+        Button bMean, bErode;
         bMean = (Button)findViewById(R.id.bMean);
+        bErode = (Button)findViewById(R.id.bErode);
+
+
         bMean.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 i.putExtra("ACTION_MODE", MEAN_BLUR);
+                startActivity(i);
+            }
+        });
+
+        bErode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                i.putExtra("ACTION_MODE", ERODE);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/qiupenglin/independent_study/MainActivity.java
+++ app/src/main/java/com/example/qiupenglin/independent_study/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.qiupenglin.independent_study;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -10,12 +14,21 @@ import android.widget.ImageView;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.InputStream;
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView ivImage, ivImageProcessed;
     private final int SELECT_PHOTO = 1;
+    Mat src;
     static int ACTION_MODE = 0;
 
 
@@ -37,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ivImage = (ImageView)findViewById(R.id.ivImage);
         ivImageProcessed = (ImageView)findViewById(R.id.ivImageProcessed);
         Intent intent = getIntent();
@@ -64,6 +77,42 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+            case SELECT_PHOTO:
+                if (requestCode == RESULT_OK) {
+                    try {
+                        //code to load image into a Bitmap and convert it to a Mat for processing
+                        final Uri imageUri = imageReturnedIntent.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        src = new Mat(selectedImage.getHeight(), selectedImage.getWidth(), CvType.CV_8UC4);
+                        Utils.bitmapToMat(selectedImage, src);
+
+                        switch (ACTION_MODE) {
+                            case HomeActivity.MEAN_BLUR:
+                                Imgproc.blur(src, src, new Size(3,3));
+                                break;
+                            case HomeActivity.ERODE:
+                                Mat kernelErode = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5, 5));
+                                Imgproc.erode(src, src, kernelErode);
+                                break;
+                        }
+                        Bitmap processedImage = Bitmap.createBitmap(src.cols(), src.rows(), Bitmap.Config.ARGB_8888);
+                        Log.d("com.example.imaType",  CvType.typeToString(src.type())+"");
+                        Utils.matToBitmap(src, processedImage);
+                        ivImage.setImageBitmap(selectedImage);
+                        ivImageProcessed.setImageBitmap(processedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
     }
 
 
