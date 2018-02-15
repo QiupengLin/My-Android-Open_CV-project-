1491fc20e58fe12b62e6669f44fab843e255c797
Qiupeng Lin <qiupeng.lin.87@gmail.com>
2018-02-15 01:21:09 -0600

    Feb 15

--- app/src/main/java/com/example/qiupenglin/independent_study/MainActivity.java
+++ app/src/main/java/com/example/qiupenglin/independent_study/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.qiupenglin.independent_study;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ivImage, ivImageProcessed;
+    private final int SELECT_PHOTO = 1;
+    static int ACTION_MODE = 0;
+
+
+    private BaseLoaderCallback mOpenCVCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    // Do work here
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        ivImage = (ImageView)findViewById(R.id.ivImage);
+        ivImageProcessed = (ImageView)findViewById(R.id.ivImageProcessed);
+        Intent intent = getIntent();
+
+        if (intent.hasExtra("ACTION_MODE")) {
+            ACTION_MODE = intent.getIntExtra("ACTION_MODE", 0);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_load_image) {
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            photoPickerIntent.setType("image/*");
+            startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mOpenCVCallBack);
     }
 }
